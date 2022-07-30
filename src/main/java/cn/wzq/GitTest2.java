0fc8e1c174474aa62ecb480ce5ebe74f64a6a2a1
wangzhiqiang <wangzhiqiangcom@163.com>
2022-07-30 21:12:57 +0800

    hot-fix commit3

--- src/main/java/cn/wzq/GitTest2.java
+++ src/main/java/cn/wzq/GitTest2.java
@@ -4,5 +4,6 @@ public class GitTest2 {
     public static void main(String[] args) {
         System.out.println("hhhhhhh~~~");
         System.out.println("嘿嘿嘿嘿嘿嘿~~~");
+        System.out.println("哈哈哈哈~~~");
     }
 }
