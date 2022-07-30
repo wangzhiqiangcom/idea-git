a632a92acbf7321b6db23eb38a77b158a0fa139c
wangzhiqiang <wangzhiqiangcom@163.com>
2022-07-30 20:38:19 +0800

    commit2

--- src/main/java/cn/wzq/GitTest2.java
+++ src/main/java/cn/wzq/GitTest2.java
@@ -2,6 +2,6 @@ package cn.wzq;
 
 public class GitTest2 {
     public static void main(String[] args) {
-        System.out.println("hhhhhhh~~~");
+        System.out.println("hhhhddhhh~~~");
     }
 }
